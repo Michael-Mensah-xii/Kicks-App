b697512569486b89a173ea94e93d4c79f5ba887c
mike <michaelmensah56211@gmail.com>
2022-12-29 03:07:13 +0000

    Initial commit

--- app/src/main/java/com/example/trainersapp/view/LoadingPage.kt
+++ app/src/main/java/com/example/trainersapp/view/LoadingPage.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -27,125 +26,131 @@ import com.example.trainersapp.ui.theme.PoppinsTypography
 
 @Composable
 fun LoadingPage() {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .background(App_purple)
+            .background(App_purple),
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            // horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top,
 
             ) {
             Image(painter = painterResource(id = R.drawable.sneaker),
-                contentDescription = "mastercard",
+                contentDescription = "sneaker",
                 modifier = Modifier.size(570.dp),
                 contentScale = ContentScale.Crop
             )
+
         }
 
+        Spacer(modifier = Modifier.heightIn(24.dp))
 
-    }
-    Box(
-        modifier = Modifier
-            .padding(top = 450.dp)
-            .padding(horizontal = 16.dp)
-    ) {
-
-        Text(
-            text = "Good shoes take you\nplaces",
+        Box(
             modifier = Modifier
-                .widthIn(90.dp)
-                .heightIn(72.dp)
-                .padding(top = 175.dp),
-            style = PoppinsTypography.h3,
-            color = Color.Black,
-            fontSize = 24.sp,
-            textAlign = TextAlign.Start,
-        )
+                .padding(top = 450.dp)
+                .padding(horizontal = 16.dp)
+        ) {
 
-        Text(
-            buildAnnotatedString {
-                withStyle(style = ParagraphStyle(lineHeight = 40.sp)) {
-                    withStyle(
-                        style = SpanStyle(
-                            color = Color.Black,
-                            fontSize = 40.sp,
-                            fontWeight = FontWeight.Bold,
-                        )
-                    ) {
-                        append("Life isn")
-                    }
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 40.sp,
-                            color = Color.White
-                        )
-                    ) {
-                        append("’t perfect\n")
-                    }
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 40.sp,
-                            color = Color.Black
-                        )
-                    ) {
-                        append("but your\n")
+            Text(
+                buildAnnotatedString {
+                    withStyle(style = ParagraphStyle(lineHeight = 40.sp)) {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Black,
+                                fontSize = 40.sp,
+                                fontWeight = FontWeight.Bold,
+                            )
+                        ) {
+                            append("Life isn")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 40.sp,
+                                color = Color.White
+                            )
+                        ) {
+                            append("’t perfect\n")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 40.sp,
+                                color = Color.Black
+                            )
+                        ) {
+                            append("but your\n")
 
-                    }
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 40.sp,
-                            color = Color.Black
-                        )
-                    ) {
-                        append("Sneakers can\n")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 40.sp,
+                                color = Color.Black
+                            )
+                        ) {
+                            append("Sneakers can\n")
 
-                    }
-                    withStyle(
-                        style = SpanStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 40.sp,
-                            color = Color.Black
-                        )
-                    ) {
-                        append("be")
+                        }
+                        withStyle(
+                            style = SpanStyle(
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 40.sp,
+                                color = Color.Black
+                            )
+                        ) {
+                            append("be")
 
+                        }
                     }
                 }
-            }
-        )
-    }
-
-
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .padding(top = 740.dp),
-    )
-    {
-        OutlinedButton(
-            modifier = Modifier
-                .heightIn(58.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(32.dp),
-            border = BorderStroke(1.dp, App_purple),
-            onClick = { /*TODO*/ })
-        {
-            Text(
-                text = "Sign Up",
-                style = PoppinsTypography.button,
-                color = App_grey,
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier,
             )
+
+        }
+        Column(modifier = Modifier
+            .fillMaxWidth()
+        ) {
+            Text(
+                text = "Good shoes take you\nplaces",
+                modifier = Modifier
+                    .widthIn(90.dp)
+                    .heightIn(72.dp)
+                    .padding(top = 630.dp)
+                    .padding(horizontal = 16.dp),
+                style = PoppinsTypography.h3,
+                color = Color.Black,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Start,
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 735.dp, bottom = 24.dp),
+        )
+        {
+            OutlinedButton(
+                modifier = Modifier
+                    .heightIn(58.dp)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(32.dp),
+                border = BorderStroke(1.dp, App_purple),
+                onClick = { /*TODO*/ })
+            {
+                Text(
+                    text = "Sign Up",
+                    style = PoppinsTypography.button,
+                    color = App_grey,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier,
+                )
+            }
         }
     }
 }
